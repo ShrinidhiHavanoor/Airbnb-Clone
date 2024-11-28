0bb7604a15e20667758f1c85b9da48e1a218c34b
Shrinidhi Havanoor <shrinidhihavanoor7@gmail.com>
2024-11-28 23:45:00 +0530

    cors error

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
-const express=require('express');
-const app=express();
-app.get('/test',(req,res)=>{
-    res.json('test Ok');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173/",
+  })
+);
+app.get("/test", (req, res) => {
+  res.json("test Ok");
 });
 
 app.listen(4000);
